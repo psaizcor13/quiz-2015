21a22503b8fe9883e4de4f4999f4553e53aaa3a3
psaizcor13 <pj.saiz13@gmail.com>
2015-05-01 20:54:34 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -9,5 +11,9 @@ router.get('/', function(req, res) {
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz' });
 });
+
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
 
 module.exports = router;
