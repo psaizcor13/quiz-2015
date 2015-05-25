4367cf9c24eb06651b324a929dcaad01a4e19c15
Unai Rosa <dr.unai94@gmail.com>
2015-05-25 11:32:50 +0200

    Registro de usuarios y gestion de cuentas

--- app.js
+++ app.js
@@ -32,8 +32,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Helpers dinamicos:
 app.use(function(req, res, next) {
 
+ if (!req.session.redir) {     // si no existe lo inicializa
+    req.session.redir = '/';
+  }
+
 // guardar path en session.redir para despues de login
-   if (!req.path.match(/\/login|\/logout/)) {
+if (!req.path.match(/\/login|\/logout|\/user/)) {
      req.session.redir = req.path;
    }
 
